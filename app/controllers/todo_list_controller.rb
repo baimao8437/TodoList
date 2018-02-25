bf14de4dffb63e26cbd1bb8f2705b5ae79eb99c1
BaiMao <f74026488@mail.ncku.edu.tw>
2018-02-25 15:54:54 +0800

    rails g model Todo text:string key:integer completed:boolean && rake db:migrate

--- app/controllers/todo_list_controller.rb
+++ app/controllers/todo_list_controller.rb
@@ -1,2 +1,20 @@
 class TodoListController < ApplicationController
+  def index
+    # @todos = todos.all
+    respond_with todos.all
+end
+
+  def create
+    # respond_with todos.create()
+  end
+
+  def update
+    todo = todos.find(params['key'])
+    todo.update_attributes(completed: params['completed'])
+    respond_with todo
+  end
+
+  def destory
+    respond_with todos.destroy
+  end
 end
