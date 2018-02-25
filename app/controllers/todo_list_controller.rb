c0ed0b4f20cb3cccda9fe76ea092e45a29342154
BaiMao <f74026488@mail.ncku.edu.tw>
2018-02-25 21:48:55 +0800

    Rubocup setting

--- app/controllers/todo_list_controller.rb
+++ app/controllers/todo_list_controller.rb
@@ -1,20 +1,23 @@
 class TodoListController < ApplicationController
-    def index
-        @todos = Todo.all
-    end
-    def create
-        Todo.create!(
-            text: params[:text],
-            key: params[:key],
-            completed: params[:completed]
-        )
-    end
-    def update
-        todo = Todo.find_by(key: params[:id])
-        todo.update({ completed: !todo.completed })
-    end
-    def destroy
-        delete_todo = Todo.find_by(key: params[:id])
-        delete_todo.destroy
-    end
+  def index
+    @todos = Todo.all
+  end
+
+  def create
+    Todo.create!(
+      text: params[:text],
+      key: params[:key],
+      completed: params[:completed]
+    )
+  end
+
+  def update
+    todo = Todo.find_by(key: params[:id])
+    todo.update(completed: !todo.completed)
+  end
+
+  def destroy
+    delete_todo = Todo.find_by(key: params[:id])
+    delete_todo.destroy
+  end
 end
